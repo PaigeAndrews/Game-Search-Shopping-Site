0a552f61b163b792ae084bd2077669fdb58af583
Paige Andrews <pandrews25@yahoo.com>
2020-01-05 23:57:08 -0500

    Testing JSON load

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ function App() {
     getGames()
   }, []);
 
+// pulling and awaiting the response from the gaming API
 const getGames = async () => {
   const response = await fetch("https://api.rawg.io/api/platforms")
   const data = await response.json()
   console.log(data)
+  console.log("Json test load.")
 }
 
-
+// Where the HTML will be written 
   return (
     <div className="searchArea">
-    <h1> Videogames Search</h1>
+      <h1> Videogames Search</h1>
       <form>
         <input type="text" placeholder="Search..."/>
         <button className="submitButton" type="submit">

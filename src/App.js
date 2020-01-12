f076094b3ff0407c4d83effb2da535b5c407ce90
Paige Andrews <pandrews25@yahoo.com>
2020-01-11 21:47:52 -0500

    added a few tachyons and classes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React, {useEffect, useState} from 'react'; 
 import Game from "./Game";
 import "./App.css";
+import { queryAllByAltText } from '@testing-library/dom';
 
 
 
 function App() {
   const [games, setGames] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
 
   useEffect (   () => {
     getGames()
-  }, []);
+  }, [query]);
 
 // pulling and awaiting the response from the gaming API
 const apiBase = "https://api.rawg.io/api/"
 const getGames = async () => {
-  const response = await fetch(`${apiBase}games`)
+  const response = await fetch(`${apiBase}games?size=200x200`)
   const data = await response.json()
   setGames(data.results);
   console.log(data)
 }
 
+const updateSearch = e => {
+  setSearch(e.target.value);
+}
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+}
+
 // Where the HTML will be written 
   return (
-    <div className="searchArea">
+    <div className="searchArea tc">
       <h1> Videogames Search</h1>
-      <form>
-        <input type="text" placeholder="Search..."/>
-        <button className="submitButton" type="submit">
+      <form onSubmit={getSearch} className= "search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="submit-button" type="submit">
           Search
         </button>
       </form>
+      <div className="flex-wrap pa6 w-90 tc">
       {games.map(game =>(
         <Game 
           key={game.name}
@@ -40,6 +53,7 @@ const getGames = async () => {
 
         />
       ))}
+      </div>
     </div>
   );
 };
